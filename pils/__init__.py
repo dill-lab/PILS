3ef751f093c12b71def5ee2ba6ed535fcce99da2
murtaza <themurtazanazir@gmail.com>
2025-05-20 11:20:25 +0530

    fix

--- pils/__init__.py
+++ pils/__init__.py
@@ -5,16 +5,6 @@ from . import (  # noqa: F401
     experiments,
     metrics,
     models,
-    prompts,
     run_args,
     trainers,
-    trainers_baseline,
 )
-from .api import (  # noqa: F401
-    invert_embeddings,
-    invert_embeddings_and_return_hypotheses,
-    invert_strings,
-    load_corrector,
-    load_pretrained_corrector,
-)
-from .trainers import Corrector  # noqa: F401
