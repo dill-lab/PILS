829776e9373b811848e1a7ae2a97ccec2df0994d
murtaza <themurtazanazir@gmail.com>
2025-05-20 10:28:25 +0530

    changed package name to pils

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="vec2text",
-    version="0.0.12",
-    description="convert embedding vectors back to text",
-    author="Jack Morris",
-    author_email="jxm3@cornell.edu",
+    name="pils",
+    version="0.0.1",
+    author="anonymous",
+    author_email="anon@myo.us",
     packages=find_packages(),
     install_requires=open("requirements.txt").readlines(),
 )
