8c919cd0de9cb673db3e06ad5fa4709321924df3
murtaza <themurtazanazir@gmail.com>
2025-05-20 13:19:06 +0530

    fix

--- pils/data_helpers.py
+++ pils/data_helpers.py
@@ -48,20 +48,6 @@ def get_world_size() -> int:
     except (RuntimeError, ValueError):
         return 1
 
-
-def load_one_million_paired_instructions() -> datasets.Dataset:
-    # has only "train" split, and "system" (system prompt)
-    # and "user" (user input) columns
-    dataset_dict = datasets.load_dataset("wentingzhao/one-million-paired-instructions")
-    dataset_dict = dataset_map_multi_worker(
-        dataset_dict,
-        map_fn=create_ompi_ex,
-        num_proc=get_num_proc(),
-    )
-
-    return dataset_dict["train"]
-
-
 def load_one_million_instructions() -> datasets.Dataset:
     # has only "train" split, and "system" (system prompt)
     # and "user" (user input) columns
@@ -69,31 +55,6 @@ def load_one_million_instructions() -> datasets.Dataset:
     dataset_dict = dataset_map_multi_worker(dataset_dict, create_ompi_ex)
 
     return dataset_dict["train"]
-
-
-def load_synthetic_gpts() -> datasets.DatasetDict:
-    custom_data_root = os.environ.get("CUSTOM_DATASET_ROOT")
-    assert custom_data_root, "`synthetic_gpts` could not be loaded. Please set the environ variable `CUSTOM_DATASET_ROOT`"
-    train_ds = datasets.load_from_disk(os.path.join(custom_data_root, "train", "synthetic_gpts"))
-    test_ds = datasets.load_from_disk(os.path.join(custom_data_root, "test", "synthetic_gpts"))
-    dataset_dict = datasets.DatasetDict()
-    dataset_dict["train"] = train_ds
-    dataset_dict["validation"] = test_ds
-
-    from transformers import AutoTokenizer
-    tokenizer = AutoTokenizer.from_pretrained("t5-base")
-
-    def decode(sample):
-        #sample["user"] = "Give me 16 short sentences that best describe yourself. Start with \"1:\""
-        sample["user"] = STATIC_USER_PROMPT
-        sample["system"] = tokenizer.decode([i for i in sample["system_prompt"] if i!= -100], skip_special_tokens=True)
-
-        sample["text"] = sample["system"] + "\n\n" + sample["user"]
-        sample["prefix"] = sample["system"] + "\n\n"
-        sample["suffix"] = sample["user"]
-        return sample
-
-    return dataset_dict.map(decode)
 
 
 def load_awesomegpt_prompts() -> datasets.DatasetDict:
@@ -149,50 +110,19 @@ def load_real_gpts() -> datasets.DatasetDict:
 
     return dataset_dict.map(decode)
 
+
 def load_anthropic_toxic_prompts() -> datasets.Dataset:
     d = datasets.load_dataset("wentingzhao/anthropic-hh-first-prompt")["train"]
     d = d.rename_column("user", "text")
     return d
 
 
-def load_luar_reddit() -> datasets.Dataset:
-    d = datasets.load_dataset("friendshipkim/reddit_eval_embeddings_luar")
-    d = d.rename_column("full_text", "text")
-    d = d.rename_column("embedding", "frozen_embeddings")
-    return d
-
-
 def dataset_from_args(data_args: DataArguments) -> datasets.DatasetDict:
     """Loads a dataset from data_args create in `run_args`."""
-    if data_args.dataset_name == "nq":
-        raw_datasets = load_nq_dpr_corpus()
-        raw_datasets["validation"] = raw_datasets["dev"]
-    elif data_args.dataset_name == "msmarco":
-        raw_datasets = load_msmarco_corpus()
-        raw_datasets = raw_datasets.train_test_split(test_size=0.01)
-        raw_datasets["validation"] = raw_datasets["test"]
-    elif data_args.dataset_name == "one_million_instructions":
+    if data_args.dataset_name == "one_million_instructions":
         raw_datasets = load_one_million_instructions()
         raw_datasets = raw_datasets.train_test_split(test_size=0.01)
         raw_datasets["validation"] = raw_datasets["test"]
-    elif data_args.dataset_name == "one_million_paired_instructions":
-        raw_datasets = load_one_million_paired_instructions()
-        raw_datasets = raw_datasets.train_test_split(test_size=0.01)
-        raw_datasets["validation"] = raw_datasets["test"]
-    elif data_args.dataset_name == "one_million_instructions":
-        raw_datasets = load_one_million_instructions()
-        raw_datasets = raw_datasets.train_test_split(test_size=0.01)
-        raw_datasets["validation"] = raw_datasets["test"]
-    elif data_args.dataset_name == "luar_reddit":
-        all_luar_datasets = load_luar_reddit()
-        raw_datasets = datasets.DatasetDict(
-            {
-                "train": all_luar_datasets["candidates"],
-                "validation": all_luar_datasets["queries"],
-            }
-        )
-    elif data_args.dataset_name == "synthetic_gpts":
-        raw_datasets = load_synthetic_gpts()
     elif data_args.dataset_name == "awesomegpt_prompts":
         raw_datasets = load_awesomegpt_prompts()
     elif data_args.dataset_name == "real_gpts":
@@ -202,138 +132,16 @@ def dataset_from_args(data_args: DataArguments) -> datasets.DatasetDict:
     return raw_datasets
 
 
-def load_ag_news_test() -> datasets.Dataset:
-    return datasets.load_dataset("ag_news")["test"]
-
-
-def load_xsum_val(col: str) -> datasets.Dataset:
-    d = datasets.load_dataset("xsum")["validation"]
-    d = d.rename_column(col, "text")
-    return d
-
-
-def load_wikibio_val() -> datasets.Dataset:
-    d = datasets.load_dataset("wiki_bio", trust_remote_code=True)["val"]
-    d = d.rename_column("target_text", "text")
-    return d
-
-
-def load_arxiv_val() -> datasets.Dataset:
-    d = datasets.load_dataset("ccdv/arxiv-summarization")["validation"]
-    d = d.rename_column("abstract", "text")
-    return d
-
-
 def load_python_code_instructions_18k_alpaca() -> datasets.Dataset:
     d = datasets.load_dataset("iamtarun/python_code_instructions_18k_alpaca")["train"]
     d = d.rename_column("instruction", "text")
     return d
 
-
-def load_beir_corpus(name: str) -> List[str]:
-    from beir import util as beir_util
-    from beir.datasets.data_loader import GenericDataLoader
-
-    #### Download scifact.zip dataset and unzip the dataset
-    beir_datasets_cache_dir = "/home/jxm3/research/retrieval/distractor_exp"
-
-    url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(
-        name
-    )
-    out_dir = os.path.join(beir_datasets_cache_dir, "datasets")
-    data_path = beir_util.download_and_unzip(url, out_dir)
-
-    # Limit each corpus to first 100k documents.
-    MAX_N = 100_000
-
-    if name == "cqadupstack":
-        full_corpus = []
-        for folder in [
-            "android",
-            "english",
-            "gaming",
-            "gis",
-            "mathematica",
-            "physics",
-            "programmers",
-            "stats",
-            "tex",
-            "unix",
-            "webmasters",
-            "wordpress",
-        ]:
-            corpus, _queries, _qrels = GenericDataLoader(
-                data_folder=os.path.join(data_path, folder)
-            ).load(split="test")
-            full_corpus.extend([k["text"] for k in corpus.values()])
-        random.shuffle(full_corpus)
-        return full_corpus[:MAX_N]
-    else:
-        corpus, _queries, _qrels = GenericDataLoader(data_folder=data_path).load(
-            split="test"
-        )
-        corpus = [k["text"] for k in corpus.values()]
-        return corpus[:MAX_N]
-
-
-def load_beir_dataset(name: str) -> datasets.Dataset:
-    cache_path = (
-        datasets.config.HF_DATASETS_CACHE
-    )  # something like /home/jxm3/.cache/huggingface/datasets
-    dataset_path = os.path.join(cache_path, "emb_inv_beir", name)
-    # print(f"loading BEIR dataset: {name}")
-    if os.path.exists(dataset_path):
-        logging.info("Loading BEIR dataset %s path %s", dataset_path)
-        dataset = datasets.load_from_disk(dataset_path)
-    else:
-        logging.info(
-            "Loading BEIR dataset %s from JSON (slow) at path %s", dataset_path
-        )
-        corpus = load_beir_corpus(name=name)
-        dataset = datasets.Dataset.from_list([{"text": t} for t in corpus])
-        os.makedirs(os.path.join(cache_path, "emb_inv_beir"), exist_ok=True)
-        dataset.save_to_disk(dataset_path)
-        logging.info("Saved BEIR dataset as HF path %s", dataset_path)
-    return dataset
-
-
-def load_beir_datasets() -> datasets.DatasetDict:
-    all_beir_datasets = [
-        ####### public datasets #######
-        "arguana",
-        "climate-fever",
-        "cqadupstack",
-        "dbpedia-entity",
-        "fever",
-        "fiqa",
-        "hotpotqa",
-        "msmarco",
-        "nfcorpus",
-        "nq",
-        "quora",
-        "scidocs",
-        "scifact",
-        "trec-covid",
-        "webis-touche2020",
-        ####### private datasets #######
-        "signal1m",
-        "trec-news",
-        "robust04",
-        "bioasq",
-    ]
-    return datasets.DatasetDict({k: load_beir_dataset(k) for k in all_beir_datasets})
-
-
 def load_standard_val_datasets() -> datasets.DatasetDict:
     """Loads a pre-defined set of standard val datasets."""
     d = {
-        #"ag_news": load_ag_news_test(),
-        #"anthropic_toxic_prompts": load_anthropic_toxic_prompts(),
-        #"arxiv": load_arxiv_val(),
-        #"python_code_alpaca": load_python_code_instructions_18k_alpaca(),
-        # "xsum_doc": load_xsum_val("document"),
-        # "xsum_summ": load_xsum_val("summary"),
-        #"wikibio": load_wikibio_val(),
+        "anthropic_toxic_prompts": load_anthropic_toxic_prompts(),
+        "python_code_alpaca": load_python_code_instructions_18k_alpaca(),
         "awesomegpt_prompts": load_awesomegpt_prompts()["validation"],
         "real_gpts": load_real_gpts()["validation"],
     }
